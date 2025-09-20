524b3f7916f60af6c4fae15554d5ef4c3ec661cc
Yassir Rouane <yasserro100@gmail.com>
2025-09-20 17:57:56 +0100

    feat: contact page

--- apps/web/src/app/(about)/contact/page.tsx
+++ apps/web/src/app/(about)/contact/page.tsx
@@ -1,7 +1,51 @@
+import { Input } from "@/components/ui/input";
 import "./style.css";
+import { ArrowUpRight } from "lucide-react";
+import Link from "next/link";
+import {
+	ItemFlipAnimation,
+	ItemFlipWrapper,
+} from "@/components/item-flip-animation";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
 const Page = () => {
-	return <p>s</p>;
+	return (
+		<div id="contact-details">
+			<form>
+				<h1>Let's Talk</h1>
+
+				<div className="inputs-group">
+					<Input placeholder="Name" name="name" />
+					<Input placeholder="Email" name="email" />
+				</div>
+
+				<Textarea placeholder="Message" name="message" />
+
+				<Button type="submit">Send Message</Button>
+			</form>
+			<section className="location-cards">
+				<div>
+					<h1>Prague</h1>
+					<p>
+						Vinohradská 121, <br /> 130 00 Praha 3, <br /> Czech Republic
+					</p>
+					<Link
+						href="https://google.com"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<ItemFlipWrapper className="button-wrapper">
+							<Button type="button">
+								Get Directions
+								<ItemFlipAnimation text={<ArrowUpRight />} />
+							</Button>
+						</ItemFlipWrapper>
+					</Link>
+				</div>
+			</section>
+		</div>
+	);
 };
 
 export default Page;

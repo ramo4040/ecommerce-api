478775efbccf69c79d108901d2a27be0dc59323f
Yassir Rouane <yasserro100@gmail.com>
2025-11-01 13:22:17 +0100

    feat: ProductsSeeder

--- apps/api/database/seeders/ProductsSeeder.php
+++ apps/api/database/seeders/ProductsSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 use App\Models\Product;
+use Illuminate\Support\Str;
 
 class ProductsSeeder extends Seeder
 {
@@ -12,6 +14,43 @@ class ProductsSeeder extends Seeder
      */
     public function run(): void
     {
-        Product::factory()->count(20)->create();
+
+        $products = [];
+
+        for ($i = 0; $i < 50; $i++) {
+            $name = fake()->unique()->word();
+            $slug = Str::slug($name);
+            $sku = strtoupper(Str::random(8));
+
+            $products[] = [
+                'name' => $name,
+                'description' => 'Made from solid oak, this chair showcases the natural beauty of the wood with its smooth grain and warm finish.',
+                'slug' => $slug,
+                'sku' => $sku,
+                'price' => '299.00',
+                'compare_price' => '400.00',
+                'quantity' => 50,
+                'status' => 'active',
+                'category_id' => Category::inRandomOrder()->first()->id,
+                'main_image' => 'products/p1.jpg',
+                'gallery_images' => json_encode([
+                    'products/p3.jpg',
+                    'products/p2.jpg'
+                ]),
+                'meta_title' => null,
+                'meta_description' => 'Elevate your dining experience with the Nordic Dining Chair, a perfect blend of timeless design and modern craftsmanship. Made from solid oak, this chair showcases the natural beauty of the wood with its smooth grain and warm finish. The ergonomic design ensures comfort for long meals and gatherings, while its minimalist silhouette complements any interior style.',
+                'tags' => json_encode([
+                    'smartphone',
+                    'iphone',
+                    'apple',
+                    'mobile',
+                    'electronics',
+                    'premium'
+                ]),
+                'is_featured' => false,
+            ];
+        }
+
+        Product::insert($products);
     }
 }

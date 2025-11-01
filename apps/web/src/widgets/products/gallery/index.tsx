cc91c13c5c1287fd7a45bdb0bfe3af12265e248a
Yassir Rouane <yasserro100@gmail.com>
2025-11-01 23:52:52 +0100

    feat: create product gallery widget for product page

---
+++ apps/web/src/widgets/products/gallery/index.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import Image from "next/image";
+import "./style.css";
+import { type ComponentProps, useCallback, useRef, useState } from "react";
+import type { Product } from "@/entities/product";
+import { cn } from "@/lib/utils";
+import { ImageObserver } from "./image-observer";
+
+type Props = ComponentProps<"div"> & {
+  product: Product;
+};
+
+export const ProductGallery = ({ product }: Props) => {
+  const [currentImage, setCurrentImage] = useState<string>(product.main_image);
+  const imageRefsMap = useRef<Map<string, HTMLDivElement>>(new Map());
+
+  const images = [product.main_image, ...(product.gallery_images || [])];
+
+  const handleRegisterRef = useCallback(
+    (imageSrc: string, element: HTMLDivElement) => {
+      imageRefsMap.current.set(imageSrc, element);
+    },
+    [],
+  );
+
+  const handleImageClick = (imageSrc: string) => {
+    const targetElement = imageRefsMap.current.get(imageSrc);
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+      setCurrentImage(imageSrc);
+    }
+  };
+
+  return (
+    <div id="product-gallery">
+      {images.map((imageSrc) => (
+        <ImageObserver
+          key={imageSrc}
+          product={product}
+          imageSrc={imageSrc}
+          setCurrentImage={setCurrentImage}
+          onRegisterRef={handleRegisterRef}
+        />
+      ))}
+
+      <div className="img-selector-wrapper">
+        <div className="img-selector">
+          {images.map((imageSrc) => (
+            <div
+              key={imageSrc}
+              className={cn(currentImage === imageSrc && "active")}
+            >
+              <Image
+                src={`${process.env.NEXT_PUBLIC_API_URL}/storage/${imageSrc}`}
+                alt={product.meta_title ?? product.name}
+                fill
+                style={{ objectFit: "cover" }}
+                onClick={() => handleImageClick(imageSrc)}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};

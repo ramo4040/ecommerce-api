4f71b5405da349622f89d1fcef93a9a031252d92
Yassir Rouane <yasserro100@gmail.com>
2025-11-02 19:16:24 +0100

    feat(shop): add CustomAccordion component

--- apps/web/src/app/shop/[product]/page.tsx
+++ apps/web/src/app/shop/[product]/page.tsx
@@ -1,3 +1,4 @@
+import CustomAccordion from "@/components/custom-accordion";
 import "./style.css";
 import { getProductBySlug, indexProducts } from "@/entities/product";
 import { ProductGallery } from "@/widgets/products/gallery";
@@ -27,6 +28,43 @@ export default async function Page({ params }: { params: Params }) {
         <div className="product-info">
           <ProductInfoDetails product={product} />
           <ProductFeaturesMarquee />
+
+          <CustomAccordion
+            collapsible
+            items={[
+              {
+                title: "Description",
+                content: <p>{product.meta_description}</p>,
+              },
+              {
+                title: "Specifications",
+                content: "",
+              },
+              {
+                title: "Shipping & Returns",
+                content: (
+                  <div className="shipping-accordion">
+                    <h1>Shipping</h1>
+                    <p>
+                      We ship your furniture securely within 14 business days.
+                      Delivery times vary by location, and tracking details are
+                      provided upon dispatch. International orders may incur
+                      customs fees.
+                    </p>
+
+                    <br />
+                    <h1>Return Policy</h1>
+                    <p>
+                      Not satisfied? Return items in their original condition
+                      within 30 days. For damaged or defective items, return
+                      shipping is free. Refunds are issued after inspection.
+                      Contact us for assistance!
+                    </p>
+                  </div>
+                ),
+              },
+            ]}
+          />
         </div>
       </section>
     </main>
